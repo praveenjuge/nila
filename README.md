d3e2fd17afcba5dc0ed394f0e1e5a76b7d342035
praveenjuge <praveen@skcript.com>
2021-05-02 18:07:11 +0530

    Update Readme
    Signed-off-by: praveenjuge <praveen@skcript.com>

--- README.md
+++ README.md
@@ -4,35 +4,9 @@ UI Elements for React with TailwindCSS
 
 [![NPM](https://img.shields.io/npm/v/nila.svg)](https://www.npmjs.com/package/nila) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
-## Install
+## Documentation
 
-```bash
-npm install nila --save
-```
-
-For proper purge and jit support for tailwind, add this in your `tailwind.config.js`:
-
-```js
-// tailwind.config.js
-module.exports = {
-  purge: [
-    // ...
-    './node_modules/nila/src/*.tsx',
-  ],
-};
-```
-
-## Usage
-
-```jsx
-import { Button } from 'nila';
-
-function App() {
-  return (
-    <Button>Button</Button>;
-  )
-}
-```
+For full documentation go to the [documentation website](https://nila.vercel.app/).
 
 ## License
 
--- docs/pages/badge.js
+++ docs/pages/badge.js
@@ -7,8 +7,8 @@ export default function Badges() {
     <ElementLayout title="Badge">
       <Code
         code="{/* Basic */}
-<Badge>...</Badge>
-<Badge rounded>...</Badge>"
+<Badge>Badge</Badge>
+<Badge rounded>Badge</Badge>"
       >
         <Badge>Badge</Badge>
         <Badge rounded>Badge</Badge>
@@ -16,14 +16,14 @@ export default function Badges() {
 
       <Code
         code='{/* Light Colors */}
-<Badge>...</Badge>
-<Badge variant="red">...</Badge>
-<Badge variant="yellow">...</Badge>
-<Badge variant="green">...</Badge>
-<Badge variant="blue">...</Badge>
-<Badge variant="indigo">...</Badge>
-<Badge variant="purple">...</Badge>
-<Badge variant="pink">...</Badge>'
+<Badge>Badge</Badge>
+<Badge variant="red">Badge</Badge>
+<Badge variant="yellow">Badge</Badge>
+<Badge variant="green">Badge</Badge>
+<Badge variant="blue">Badge</Badge>
+<Badge variant="indigo">Badge</Badge>
+<Badge variant="purple">Badge</Badge>
+<Badge variant="pink">Badge</Badge>'
       >
         <Badge>Badge</Badge>
         <Badge variant="red">Badge</Badge>
@@ -37,14 +37,14 @@ export default function Badges() {
 
       <Code
         code='{/* Solid Colors */}
-<Badge solid>...</Badge>
-<Badge solid variant="red">...</Badge>
-<Badge solid variant="yellow">...</Badge>
-<Badge solid variant="green">...</Badge>
-<Badge solid variant="blue">...</Badge>
-<Badge solid variant="indigo">...</Badge>
-<Badge solid variant="purple">...</Badge>
-<Badge solid variant="pink">...</Badge>'
+<Badge solid>Badge</Badge>
+<Badge solid variant="red">Badge</Badge>
+<Badge solid variant="yellow">Badge</Badge>
+<Badge solid variant="green">Badge</Badge>
+<Badge solid variant="blue">Badge</Badge>
+<Badge solid variant="indigo">Badge</Badge>
+<Badge solid variant="purple">Badge</Badge>
+<Badge solid variant="pink">Badge</Badge>'
       >
         <Badge solid>Badge</Badge>
         <Badge solid variant="red">
